2fee354299ea6098926b253ae4e32622357f7df2
KwonSeongHyeok <aiqksiwk@naver.com>
2025-05-14 23:58:06 +0900

    state

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -4,16 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   let post = '강남 우동 맛집';
+  let [글제목, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  // let [a, b] = useState('남자 코트 추천')
+  // a = state에 보관한 자료가 나옴
+  // b = state 변경도와주는 함수
+  // 변동시 자동으로 html에 반영되게 만들고 싶을 때 state 사용
+  // 자주 변경될거같은 html 부분은 state로 만들기
 
   return (
     <div className='App'>
       <div className='black-nav'>
-        <h4 style={ {color : 'red', fontSize : '16px'} }>블로그임</h4>
+        <h4>Today-It Blog</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className='list'>
+        <h4>{글제목[0]}</h4>
+        <p>5월 14일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>5월 14일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>5월 14일 발행</p>
+      </div>
     </div>
   )
 }
