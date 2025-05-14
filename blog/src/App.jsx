8a00992951b40b3c2a6bc3de5b5566e5e1747d59
KwonSeongHyeok <aiqksiwk@naver.com>
2025-05-14 23:07:41 +0900

    react start

---
+++ blog/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let post = '강남 우동 맛집';
+
+  return (
+    <div className='App'>
+      <div className='black-nav'>
+        <h4 style={ {color : 'red', fontSize : '16px'} }>블로그임</h4>
+      </div>
+      <h4>{ post }</h4>
+    </div>
+  )
+}
+
+export default App
