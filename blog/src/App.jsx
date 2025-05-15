3e0b31952f24f5f2fec8f162b0e1d068229d891f
KwonSeongHyeok <aiqksiwk@naver.com>
2025-05-15 23:06:21 +0900

    state변경

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */ // Ling끄는 기능
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -12,6 +14,17 @@ function App() {
   // b = state 변경도와주는 함수
   // 변동시 자동으로 html에 반영되게 만들고 싶을 때 state 사용
   // 자주 변경될거같은 html 부분은 state로 만들기
+  let [따봉, 따봉변경] = useState(0);
+  // state 변경하는 법
+  // 등호로는 변경 X
+  // state변경함수(새로운 state)
+  // ex 따봉변경(1)
+
+  // onClick에는 함수 이름을 넣어야함
+  // onClick={ function(){} } 이것도 가능 또는 () => {} 이것도 가능
+  function 함수(){ // 긴 코드를 한 단어로 묶어주는 문법
+    console.log(1);
+  }
 
   return (
     <div className='App'>
@@ -19,7 +32,7 @@ function App() {
         <h4>Today-It Blog</h4>
       </div>
       <div className='list'>
-        <h4>{글제목[0]}</h4>
+        <h4>{ 글제목[0] } <span onClick={() => {따봉변경(따봉+1)} }>👍</span> {따봉} </h4>
         <p>5월 14일 발행</p>
       </div>
       <div className='list'>
